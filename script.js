8fc033a56628826709934a9f3f2c1510433d276e
kaio <kaio.devfull@gmail.com>
2024-08-22 20:20:33 -0300

    Atualizando

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-async function enviarAgendamento() {
+// Função para enviar os dados para o WhatsApp
+function enviarParaWhatsApp() {
     const nome = document.getElementById('nome').value;
     const telefone = document.getElementById('telefone').value;
     const data = document.getElementById('data').value;
     const horario = document.getElementById('horario').value;
     const barbeiro = document.getElementById('barbeiro').value;
 
-    const agendamentoData = { 
-      acao: 'agendar', // Adicione "acao" para identificar o método
-      nome, 
-      telefone, 
-      data, 
-      horario, 
-      barbeiro 
-    };
-  
-    try {
-        const response = await fetch('/agendamento', { // Endpoint do servlet
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(agendamentoData),
+    const mensagem = `Olá, gostaria de marcar este horário!! %0A%0A*Novo Agendamento*%0A*Nome*: ${nome}%0A*Telefone*: ${telefone}%0A*Data*: ${data}%0A*Horário*: ${horario}%0A*Barbeiro*: ${barbeiro}`;
+    const numeroWhatsApp = '5548988114792';  // Substitua pelo número da barbearia com código do país (55 para Brasil).
+
+    const url = `https://api.whatsapp.com/send?phone=${numeroWhatsApp}&text=${mensagem}`;
+
+    window.open(url, '_blank');
+}
+
+// Função para preencher o campo de horário com as opções disponíveis
+async function carregarHorariosDisponiveis() {
+    const data = document.getElementById('data').value;
+    if (data) {
+        const response = await fetch(`/horarios-disponiveis?data=${data}`);
+        const horariosDisponiveis = await response.json();
+        
+        const selectHorario = document.getElementById('horario');
+        selectHorario.innerHTML = ''; // Limpa os horários atuais
+
+        horariosDisponiveis.forEach(horario => {
+            const option = document.createElement('option');
+            option.value = horario;
+            option.text = horario;
+            selectHorario.appendChild(option);
         });
-
-        if (!response.ok) {
-            const errorMessage = await response.text();
-            throw new Error(errorMessage);
-        }
-
-        alert('Agendamento realizado com sucesso!');
-
-    } catch (error) {
-        alert('Erro ao realizar agendamento: ' + error.message);
     }
 }
+
+document.getElementById('data').addEventListener('change', carregarHorariosDisponiveis);
